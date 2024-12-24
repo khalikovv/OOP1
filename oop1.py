af87d4d0626b8e755d5fda3f9174c8d3db3be411
khalikovv <154545842+khalikovv@users.noreply.github.com>
2024-12-25 01:46:49 +0300

    Update oop1.py

--- oop1.py
+++ oop1.py
@@ -3,48 +3,48 @@ import json
 import webbrowser
 import urllib.parse
 
-def search_wikipedia(query):
-    # URL-кодирование запроса
-    encoded_query = urllib.parse.quote(query)
-    # Формирование URL для запроса к API Википедии
-    url = f"https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch={encoded_query}"
+class WikipediaSearch:
+    def __init__(self):
+        self.base_url = "https://ru.wikipedia.org/w/api.php"
 
-    # Выполнение запроса
-    response = requests.get(url)
-    data = response.json()
+    def search_articles(self, query):
+        # URL-кодирование запроса
+        encoded_query = urllib.parse.quote(query)
+        # Формирование URL для запроса к API Википедии
+        url = f"{self.base_url}?action=query&list=search&utf8=&format=json&srsearch={encoded_query}"
 
-    # Получение результатов поиска
-    search_results = data['query']['search']
-    return search_results
+        # Выполнение запроса
+        response = requests.get(url)
+        data = response.json()
 
-def display_search_results(results):
-    print("Результаты поиска:")
-    for i, result in enumerate(results, start=1):
-        print(f"{i}. {result['title']}")
+        # Получение результатов поиска
+        return data['query']['search']
 
-def open_wikipedia_page(pageid):
-    url = f"https://ru.wikipedia.org/w/index.php?curid={pageid}"
-    webbrowser.open(url)
+    def display_search_result(self, results):
+        print("Результаты поиска:")
+        for i, result in enumerate(results, start=1):
+            print(f"{i}. {result['title']}")
 
-def main():
+    def open_article_page(self, pageid):
+        url = f"https://ru.wikipedia.org/w/index.php?curid={pageid}"
+        webbrowser.open(url)
+
+if __name__ == "__main__":
+    app = WikipediaSearch()
     query = input("Введите поисковый запрос: ")
-    results = search_wikipedia(query)
+    results = app.search_articles(query)
 
     if not results:
         print("Ничего не найдено.")
-        return
+    else:
+        app.display_search_result(results)
 
-    display_search_results(results)
-
-    try:
-        choice = int(input("Введите номер статьи для открытия: "))
-        if 1 <= choice <= len(results):
-            selected_result = results[choice - 1]
-            open_wikipedia_page(selected_result['pageid'])
-        else:
-            print("Неверный выбор.")
-    except ValueError:
-        print("Неверный ввод. Пожалуйста, введите число.")
-
-if __name__ == "__main__":
-    main()
+        try:
+            choice = int(input("Введите номер статьи для открытия: "))
+            if 1 <= choice <= len(results):
+                selected_result = results[choice - 1]
+                app.open_article_page(selected_result['pageid'])
+            else:
+                print("Неверный выбор.")
+        except ValueError:
+            print("Неверный ввод. Пожалуйста, введите число.")
